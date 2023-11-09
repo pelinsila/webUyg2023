315f6b5f2c8a4460c4d886aba73b3ebb9613c21f
pelinsila <pelinncengiz@gmail.com>
2023-11-09 15:18:01 +0300

    login bağlantısı yapıldı

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const db_connect=require('./db/mysql_connect')
+const router=require('./routers')
 const app = express()
 require('dotenv/config')
 app.get('/', function (req, res) {
@@ -9,4 +10,6 @@ app.get('/app', function (req, res) {
     res.send('app sayfası')
   })
 
+app.use(express.json({limit:'50mb',extended:true,parameterLimit:50000}))
+app.use('/api',router)
 app.listen(process.env.PORT)
